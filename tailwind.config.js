5938c726b148aee6cdfe0ef89ada01c67c6eeabb
Rizz-33 <risiniamarathunga@gmail.com>
2025-02-06 14:53:09 +0530

    refactor: extend Tailwind CSS theme with custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#8E0D3C", // Deep Red
+        accent: "#EF3B33", // Vibrant Orange
+        background: "#F7B195", // Peach
+        secondary: "#1D1842", // Blackcurrant
+      },
+    },
   },
   plugins: [],
 };
