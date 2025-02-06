16c3267f3995b22ca86a4181b39722a8322a84ae
Rizz-33 <risiniamarathunga@gmail.com>
2025-02-06 16:37:36 +0530

    feat: implement routing in App component with Home, Login, and Signup pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
 
 function App() {
   return (
-    <>
-      <div className="min-h-screen bg-white text-black flex items-center justify-center relative overflow-hidden">
-        {/* Removed FloatingShape components */}
-      </div>
-    </>
+    <div className="min-h-screen bg-white text-black flex items-center justify-center relative overflow-hidden">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
   );
 }
 
