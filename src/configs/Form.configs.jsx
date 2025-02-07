fedabe0634603a42f19d07afabad0ff0f534e377
Rizz-33 <risiniamarathunga@gmail.com>
2025-02-07 12:20:56 +0530

    feat: add configuration for customer signup form and headings

---
+++ src/configs/Form.configs.jsx
@@ -0,0 +1,32 @@
+export const footerConfigs = {
+  customerSignup: {
+    loginSignupRedirect: {
+      text: "Already have an account?",
+      link: "/login",
+      linkText: "Login",
+    },
+    terms: {
+      text: "By signing up, you agree to our",
+      link: "/terms",
+      linkText: "Terms of Service",
+    },
+    privacy: {
+      link: "/privacy",
+      linkText: "Privacy Policy",
+    },
+    alternateSignup: {
+      text: "Tailor shop?",
+      link: "/tailor-signup",
+      linkText: "Request to sign up as a Tailor Shop",
+    },
+  },
+};
+
+export const headingConfigs = {
+  customerSignup: {
+    heading1:
+      "Looking for the perfect fit? Find a tailor that meets your needs.",
+    heading2:
+      "Sign up to browse profiles of expert tailors, design, view reviews, and order your custom tailoring items.",
+  },
+};
