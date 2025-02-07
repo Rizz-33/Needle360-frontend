05640b94bf28a17ccce9cfc4817692481c4aec86
Rizz-33 <risiniamarathunga@gmail.com>
2025-02-07 22:42:37 +0530

    feat: simplify Login component state by removing unused fields and adding form type for customer login

--- src/pages/customers/Login.jsx
+++ src/pages/customers/Login.jsx
@@ -4,24 +4,12 @@ import { footerConfigs, headingConfigs } from "../../configs/Form.configs";
 
 const Login = () => {
   const [values, setValues] = useState({
-    name: "",
     email: "",
     password: "",
-    confirmPassword: "",
-    contactNumber: "",
-    country: "",
-    province: "",
-    city: "",
-    postalCode: "",
-    streetAddress: "",
-    bankDetails: "",
   });
 
   const [errors, setErrors] = useState({});
-  const [disabled, setDisabled] = useState({
-    // Example: Set fields you want to disable/hide
-    // bankDetails: true,
-  });
+  const [disabled, setDisabled] = useState({});
 
   const handleChange = (e) => {
     setValues({ ...values, [e.target.name]: e.target.value });
@@ -32,7 +20,7 @@ const Login = () => {
   };
 
   return (
-    <div className="flex w-full h-screen overflow-auto bg-gradient-to-tr from-white to-blue-50">
+    <div className="flex w-full h-screen overflow-auto bg-gradient-to-tr from-white to-blue-50 justify-center items-center">
       <div className="w-1/2 hidden md:block">
         <img
           src="/api/placeholder/800/600"
@@ -42,6 +30,7 @@ const Login = () => {
       </div>
       <div className="w-full md:w-1/2 p-4 mt-9 pr-24">
         <Form
+          formType={"customerLogin"}
           values={values}
           onChange={handleChange}
           onSubmit={handleSubmit}
