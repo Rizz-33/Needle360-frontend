93298c173bdbd0258f3dea12bf080f4451ca522a
Rizz-33 <risiniamarathunga@gmail.com>
2025-02-09 20:45:42 +0530

    feat: remove Button component from Home page and simplify return structure

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,11 @@
 import React from "react";
-import Button from "../components/Button";
 
 const Home = () => {
   const handleClick = () => {
     alert("Button clicked!");
   };
 
-  return (
-    <div>
-      <Button onClick={handleClick} className="custom-class">
-        Click Me
-      </Button>
-    </div>
-  );
+  return <div></div>;
 };
 
 export default Home;
