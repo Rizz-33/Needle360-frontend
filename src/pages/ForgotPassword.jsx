350ceac7e5ffa47a41e99bc80dfb8433344048e5
Rizz-33 <risiniamarathunga@gmail.com>
2025-02-11 17:31:20 +0530

    feat: enhance ForgotPassword component with email state and navigation button

--- src/pages/ForgotPassword.jsx
+++ src/pages/ForgotPassword.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { CustomButton } from "../components/Button";
 import Form from "../components/Form";
 import { footerConfigs, headingConfigs } from "../configs/Form.configs";
 import { useAuthStore } from "../store/Auth.store";
@@ -6,8 +7,8 @@ import { useAuthStore } from "../store/Auth.store";
 export default function ForgotPassword() {
   const [values, setValues] = useState({}); // Define values state
   const [errors, setErrors] = useState({}); // Define errors state
-  const [disabled] = useState(false); // Define disabled state
   const [isSubmitted, setIsSubmitted] = useState(false); // Define isSubmitted state
+  const [email, setEmail] = useState(""); // Define email state
 
   const { forgotPassword } = useAuthStore();
 
@@ -17,6 +18,9 @@ export default function ForgotPassword() {
       ...values,
       [name]: value,
     });
+    if (name === "email") {
+      setEmail(value);
+    }
   };
 
   console.log("Footer Config:", footerConfigs.forgotPassword.loginRedirect);
@@ -55,21 +59,27 @@ export default function ForgotPassword() {
             showAlternateSignup={false}
           />
         ) : (
-          <Form
-            formType={"resetPassword"}
-            values={values}
-            onChange={handleChange}
-            onSubmit={handleSubmit}
-            errors={errors}
-            disabled={disabled}
-            button="Reset Password"
-            heading1={headingConfigs.forgotPassword.heading1}
-            heading2={headingConfigs.forgotPassword.heading2}
-            footerConfig={footerConfigs.forgotPassword}
-            showDivider={false}
-            showTerms={false}
-            showAlternateSignup={false}
-          />
+          <div>
+            <h2 className="text-sm font-bold text-left text-primary">
+              Forgot Password
+            </h2>
+            <p className="text-xs font-light text-left pb-6">
+              An email has been sent to {email} with instructions to reset your
+              password.
+            </p>
+            <CustomButton
+              text="Go Back to Login"
+              color="primary"
+              hover_color="hoverAccent"
+              variant="outlined"
+              width="w-full"
+              height="h-9"
+              type="submit"
+              onClick={() => {
+                window.location.href = "/login";
+              }}
+            />
+          </div>
         )}
       </div>
     </div>
