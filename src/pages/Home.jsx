f6499add7d5b0644d83c382e897ce0d5e709d6f4
Rizz-33 <risiniamarathunga@gmail.com>
2025-02-12 23:21:54 +0530

    feat: add HeroSection component to Home page and enhance Tailwind CSS configuration with custom background utilities #2

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 "use client";
 
 import React from "react";
+import HeroSection from "../components/Hero";
 import NavbarMenu from "../components/Navbar-menu";
 
 const Home = () => {
   return (
-    <div className="flex w-full h-screen justify-center items-center bg-gradient-to-tr from-white to-blue-50">
-      <div className="w-full h-screen">
+    <div className="flex w-full justify-center items-center bg-gradient-to-bl from-white to-blue-50 bg-grid-gray-300/[0.2] overflow-y-auto">
+      <div className="w-full bg-gradient-to-tr from-transparent to-white">
         <NavbarMenu />
+        <HeroSection />
       </div>
     </div>
   );
