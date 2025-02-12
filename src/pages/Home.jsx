30c0540c5a3515ce333c0f91ed8133d90bf47550
Rizz-33 <risiniamarathunga@gmail.com>
2025-02-12 14:25:10 +0530

    feat: integrate NavbarMenu component into Home page layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,16 @@
+"use client";
+
 import React from "react";
+import NavbarMenu from "../components/Navbar-menu";
 
 const Home = () => {
-  const handleClick = () => {
-    alert("Button clicked!");
-  };
-
-  return <div></div>;
+  return (
+    <div className="flex w-full h-screen justify-center items-center bg-gradient-to-tr from-white to-blue-50">
+      <div className="w-full h-screen">
+        <NavbarMenu />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
