98ff67a6767299ec68709a7d4524e7692896bb89
Rizz-33 <risiniamarathunga@gmail.com>
2025-02-17 11:31:06 +0530

    feat: add LongMarquee component to Home page for enhanced visual appeal

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,16 @@
 
 import React from "react";
 import HeroSection from "../components/Hero";
+import LongMarquee from "../components/LongMarquee";
 import NavbarMenu from "../components/Navbar-menu";
 
 const Home = () => {
   return (
-    <div className="flex w-full justify-center items-center bg-gradient-to-bl from-white to-blue-50 bg-grid-gray-300/[0.2] overflow-y-auto">
-      <div className="w-full bg-gradient-to-tr from-transparent to-white">
+    <div className="flex w-full justify-center items-center bg-gradient-to-bl from-white to-blue-50 bg-grid-gray-300/[0.2] overflow-y-auto h-screen">
+      <div className="w-full bg-gradient-to-tr from-transparent to-white overflow-y-auto">
         <NavbarMenu />
         <HeroSection />
+        <LongMarquee />
       </div>
     </div>
   );
