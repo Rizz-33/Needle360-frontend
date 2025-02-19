287141a3633d5c635b9a38d44400eda1edcefd2f
Rizz-33 <risiniamarathunga@gmail.com>
2025-02-19 11:15:20 +0530

    feat: integrate DesignerCanvas component into Design page layout

--- src/pages/Design.jsx
+++ src/pages/Design.jsx
@@ -1,16 +1,18 @@
 "use client";
 
 import React from "react";
+import DesignerCanvas from "../components/design/DesignerCanvas";
 import Footer from "../components/ui/Footer";
 import NavbarMenu from "../components/ui/Navbar-menu";
 
 const Design = () => {
   return (
-    <div className="h-screen overflow-y-auto">
+    <div className="h-screen overflow-y-auto w-full">
       <div className="min-h-screen w-full bg-gradient-to-bl from-white to-blue-50 bg-grid-gray-300/[0.2]">
         <div className="w-full min-h-screen bg-gradient-to-tr from-transparent to-white pt-24">
           <NavbarMenu />
-          <div>
+          <div className="w-full">
+            <DesignerCanvas />
             <Footer />
           </div>
         </div>
@@ -18,5 +20,3 @@ const Design = () => {
     </div>
   );
 };
-
-export default Design;
